1357f1450381a2164f0e9b1f5ec349973d89b648
rom <filpoyma@gmail.com>
2025-05-05 14:53:17 +0700

    access token. refresj token

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,61 +1,62 @@
-import express from 'express';
-import { body } from 'express-validator';
-import { 
+import express from "express";
+import { body } from "express-validator";
+import {
   registerUser,
   loginUser,
   refreshToken,
   getUserProfile,
-  updateUserProfile
-} from '../controllers/authController.js';
-import { protect } from '../middleware/authMiddleware.js';
-import { validate } from '../middleware/validationMiddleware.js';
+  updateUserProfile,
+} from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+import { validate } from "../middleware/validationMiddleware.js";
 
 const router = express.Router();
 
 router.post(
-  '/',
+  "/register",
   [
-    body('name').notEmpty().withMessage('Name is required'),
-    body('email').isEmail().withMessage('Please include a valid email'),
-    body('password')
+    body("name").notEmpty().withMessage("Name is required"),
+    body("email").isEmail().withMessage("Please include a valid email"),
+    body("password")
       .isLength({ min: 6 })
-      .withMessage('Password must be at least 6 characters')
+      .withMessage("Password must be at least 6 characters"),
   ],
   validate,
   registerUser
 );
 
 router.post(
-  '/login',
+  "/login",
   [
-    body('email').isEmail().withMessage('Please include a valid email'),
-    body('password').exists().withMessage('Password is required')
+    body("email").isEmail().withMessage("Please include a valid email"),
+    body("password").exists().withMessage("Password is required"),
   ],
   validate,
   loginUser
 );
 
 router.post(
-  '/refresh-token',
-  [
-    body('refreshToken').notEmpty().withMessage('Refresh token is required')
-  ],
+  "/refresh-token",
+  [body("refreshToken").notEmpty().withMessage("Refresh token is required")],
   validate,
   refreshToken
 );
 
 router
-  .route('/profile')
+  .route("/profile")
   .get(protect, getUserProfile)
   .put(
     protect,
     [
-      body('name').optional(),
-      body('email').optional().isEmail().withMessage('Please include a valid email'),
-      body('password')
+      body("name").optional(),
+      body("email")
+        .optional()
+        .isEmail()
+        .withMessage("Please include a valid email"),
+      body("password")
         .optional()
         .isLength({ min: 6 })
-        .withMessage('Password must be at least 6 characters')
+        .withMessage("Password must be at least 6 characters"),
     ],
     validate,
     updateUserProfile
