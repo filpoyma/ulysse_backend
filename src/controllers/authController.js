ebf3a0245aa99061398d79fafeb06dc8dbdda477
rom <filpoyma@gmail.com>
2025-05-05 15:56:52 +0700

    access token. refresj token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
-import asyncHandler from 'express-async-handler';
-import jwt from 'jsonwebtoken';
-import User from '../models/userModel.js';
-import ApiError from '../utils/apiError.js';
-import config from '../config/config.js';
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import User from "../models/userModel.js";
+import ApiError from "../utils/apiError.js";
+import config from "../config/config.js";
 
 const generateTokens = (userId) => {
   const accessToken = jwt.sign({ id: userId }, config.jwtSecret, {
-    expiresIn: '15m'
+    expiresIn: "15m",
   });
-  
+
   const refreshToken = jwt.sign({ id: userId }, config.jwtSecret, {
-    expiresIn: '7d'
+    expiresIn: "7d",
   });
 
   return { accessToken, refreshToken };
@@ -21,24 +21,24 @@ export const registerUser = asyncHandler(async (req, res) => {
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    throw new ApiError(400, 'User already exists');
+    throw new ApiError(400, "User already exists");
   }
 
   const user = await User.create({
     name,
     email,
-    password
+    password,
   });
 
   if (user) {
     const { accessToken, refreshToken } = generateTokens(user._id);
-    
+
     // Set refresh token in HTTP-only cookie
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: config.isProduction,
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.status(201).json({
@@ -48,11 +48,11 @@ export const registerUser = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        accessToken
-      }
+        accessToken,
+      },
     });
   } else {
-    throw new ApiError(400, 'Invalid user data');
+    throw new ApiError(400, "Invalid user data");
   }
 });
 
@@ -63,13 +63,13 @@ export const loginUser = asyncHandler(async (req, res) => {
 
   if (user && (await user.matchPassword(password))) {
     const { accessToken, refreshToken } = generateTokens(user._id);
-    
+
     // Set refresh token in HTTP-only cookie
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: config.isProduction,
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.json({
@@ -79,11 +79,11 @@ export const loginUser = asyncHandler(async (req, res) => {
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin,
-        accessToken
-      }
+        accessToken,
+      },
     });
   } else {
-    throw new ApiError(401, 'Invalid email or password');
+    throw new ApiError(401, "Invalid email or password");
   }
 });
 
@@ -91,7 +91,7 @@ export const refreshToken = asyncHandler(async (req, res) => {
   const refreshToken = req.cookies.refreshToken;
 
   if (!refreshToken) {
-    throw new ApiError(401, 'Refresh token is required');
+    throw new ApiError(401, "Refresh token is required");
   }
 
   try {
@@ -99,39 +99,40 @@ export const refreshToken = asyncHandler(async (req, res) => {
     const user = await User.findById(decoded.id);
 
     if (!user) {
-      throw new ApiError(401, 'User not found');
+      throw new ApiError(401, "User not found");
     }
 
     const tokens = generateTokens(user._id);
-    
+
     // Set new refresh token in HTTP-only cookie
-    res.cookie('refreshToken', tokens.refreshToken, {
+    res.cookie("refreshToken", tokens.refreshToken, {
       httpOnly: true,
       secure: config.isProduction,
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      sameSite: "strict",
+      path: "/",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.json({
       success: true,
       data: {
-        accessToken: tokens.accessToken
-      }
+        accessToken: tokens.accessToken,
+      },
     });
   } catch (error) {
-    throw new ApiError(401, 'Invalid refresh token');
+    throw new ApiError(401, "Invalid refresh token");
   }
 });
 
 export const logout = asyncHandler(async (req, res) => {
-  res.cookie('refreshToken', '', {
+  res.cookie("refreshToken", "", {
     httpOnly: true,
-    expires: new Date(0)
+    expires: new Date(0),
   });
 
   res.json({
     success: true,
-    message: 'Logged out successfully'
+    message: "Logged out successfully",
   });
 });
 
@@ -145,11 +146,11 @@ export const getUserProfile = asyncHandler(async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
-        isAdmin: user.isAdmin
-      }
+        isAdmin: user.isAdmin,
+      },
     });
   } else {
-    throw new ApiError(404, 'User not found');
+    throw new ApiError(404, "User not found");
   }
 });
 
@@ -159,7 +160,7 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
   if (user) {
     user.name = req.body.name || user.name;
     user.email = req.body.email || user.email;
-    
+
     if (req.body.password) {
       user.password = req.body.password;
     }
@@ -168,11 +169,11 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
     const { accessToken, refreshToken } = generateTokens(updatedUser._id);
 
     // Set refresh token in HTTP-only cookie
-    res.cookie('refreshToken', refreshToken, {
+    res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
       secure: config.isProduction,
-      sameSite: 'strict',
-      maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
+      sameSite: "strict",
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
     res.json({
@@ -182,10 +183,10 @@ export const updateUserProfile = asyncHandler(async (req, res) => {
         name: updatedUser.name,
         email: updatedUser.email,
         isAdmin: updatedUser.isAdmin,
-        accessToken
-      }
+        accessToken,
+      },
     });
   } else {
-    throw new ApiError(404, 'User not found');
+    throw new ApiError(404, "User not found");
   }
 });
